3070110cb5f5aeb9f39ea73f11000cc90499da5e
Stephen McCready <stephenmccready@gmail.com>
2017-12-10 11:58:10 +0800

    Create psl-config.php

---
+++ includes/psl-config.php
@@ -0,0 +1,8 @@
+<?php
+define("HOST", "*** put your database location here ***");
+define("USER", "*** put your username here ***");
+define("PASSWORD", "*** put your username here ***"); 
+define("DATABASE", "*** put your password here ***");
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "user");
+define("SECURE", TRUE);
