35665f0a19d18f4362784f9f2c9f4ac75c9a46bf
Stephen McCready <stephenmccready@gmail.com>
2018-01-02 09:17:53 +0800

    Update flash.js
    Fixed bug when reaching last card of a deck

--- js/flash.js
+++ js/flash.js
@@ -1,14 +1,19 @@
+
 var resultArray;
-var randID=0;
-var lastID=0;
+var ID=0;
+var deletedID=0;
+var deletedRow;
 function LoadFlashCards() {
 	var request = new XMLHttpRequest();
 	request.onreadystatechange = function() {
 		if (request.readyState === 4) {
 			if (request.status === 200) {
 				var result = request.responseText;
+				var resultLen = result.length;
+				resultLen=resultLen-1;
+				result = result.substr(0, resultLen);
 				resultArray = result.split("|");
-				randID=0;
+				ID=0;
 				displayCard('first');
 			}
 		}
@@ -18,37 +23,40 @@ function LoadFlashCards() {
 	request.send();
 }
 function displayCard(action) {
-	var arrLen = resultArray.length-1;
+	var arrLen=resultArray.length;
+	$('#HanziS').text("");
 	if(arrLen==0) {
 		alert("Completed");
 		LoadFlashCards();
 	} else {
 		if(action=="prev"){
-			if(randID!=0){
-				randID=randID-1;
+			if(ID!=0){
+				ID--;
 			}
+			$('#debug').text("prev: " + ID.toString() + " " + arrLen.toString());		
 		} else if(action=="next"){
-			if(randID<arrLen){
-				randID=randID+1;
+			if((ID+1) < arrLen){
+				ID++;
 			}
+			$('#debug').text("next: " + ID.toString() + " " + arrLen.toString());			
 		} else if(action=="last"){
-			randID=arrLen-1;
+			ID=arrLen-1;
 		} else if(action=="first") {
-			randID=0;
+			ID=0;
 		} else if($('input[name=chkRANDOM]:checked').val()) {
-			randID=Math.floor(Math.random() * arrLen);
-			if (arrLen!=0 && randID==lastID) {
-				if(randID==0) {randID++;} else {randID--;};
+			ID=Math.floor(Math.random() * arrLen);
+			if (arrLen!=0 && ID==lastID) {
+				if(ID==0) {ID++;} else {ID--;};
 			}
+		} else if(action=="current"){
 		} else {
-			randID++;
-			if(randID >= arrLen) {
-				randID=0;
+			ID++;
+			if(ID >= arrLen) {
+				ID=0;
 			}
 		}
-		var arrLen = resultArray.length-1;
-		var row=resultArray[randID];
-		lastID=randID;
+		var row=resultArray[ID];
+		lastID=ID;
 		var col=row.split("~");
 		$('#HanziS').text(col[1]);
 		$('#Pinyin').text(col[3]);
@@ -56,16 +64,18 @@ function displayCard(action) {
 		$('#PartofSpeech').text(col[6]);
 		col[8]++;
 		$('#Views').text('Views: ' + col[8]);
-		$('#footLeft').text((randID+1).toString() + ' of ' + arrLen);
-		resultArray[randID]=col[0]+'~'+col[1]+'~'+col[2]+'~'+col[3]+'~'+col[4]+'~'+col[5]+'~'+col[6]+'~'+col[7]+'~'+col[8]+'~';
+		var dispID=ID+1;
+		$('#footLeft').text(dispID.toString() + ' of ' + arrLen.toString());
+		resultArray[ID]=col[0]+'~'+col[1]+'~'+col[2]+'~'+col[3]+'~'+col[4]+'~'+col[5]+'~'+col[6]+'~'+col[7]+'~'+col[8]+'~';
 		if($('input[name=chkPINYIN]:checked').val()) {$('#Pinyin').show();} else {$('#Pinyin').hide();};
 		if($('input[name=chkENGLISH]:checked').val()) {$('#English').show();} else {$('#English').hide();};
 		if($('input[name=chkENGLISH]:checked').val()) {$('#PartofSpeech').show();} else {$('#PartofSpeech').hide();};
 	}
 }
 function knowIt() {
-	resultArray.splice(randID, 1);
-	displayCard();
+	resultArray.splice(ID, 1);
+	if(ID!=0) {ID--;};
+	displayCard("current");
 }
 function pinyinChecked() {
 	if($('input[name=chkPINYIN]:checked').val()) {
