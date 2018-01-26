1ec6416b0ec1ca81e9ad9f50886a357392046f80
Stephen McCready <stephenmccready@gmail.com>
2018-01-26 21:03:56 +0800

    Update flash.php
    Added cookies, fixed some bugs

--- flash.php
+++ flash.php
@@ -8,21 +8,28 @@ include_once 'includes/db_connect.php';
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Flashcards</title>
 	<link type="text/css" rel="stylesheet" href="https://ajax.aspnetcdn.com/ajax/bootstrap/3.3.6/css/bootstrap.min.css">
-  	<link type="text/css" rel="stylesheet" href="css/flash.css">
+	<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.12.2.min.js"></script>
+	<script src="https://ajax.aspnetcdn.com/ajax/bootstrap/3.3.6/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/cookieconsent.min.css" />
+	<script src="js/cookieconsent.min.js"></script>
+	<!--[if IE]>
+	<script src="https://stephenmccready.asia/html5shiv.min.js"></script>
+	<![endif]-->
 </head>
-<body onload="LoadFlashCards();">
+<body onload="init();">
 <div class="wrapper">
 	<div class="col-xs-12 col-s-6 col">HSK&nbsp;
-	<select id="selHSK" onchange="$('#Pinyin').hide();$('#English').hide();LoadFlashCards();">
+	<select id="selHSK" onchange="hskChange();">
+		<option value="All">All</option>
 		<option value="1">1</option>
 		<option value="2">2</option>
-		<option value="3" selected="selected">3</option>
+		<option value="3">3</option>
 		<option value="4">4</option>
 		<option value="5">5</option>
 		<option value="6">6</option>
-		<option value="All">All</option>
 	</select>&nbsp;&nbsp;Lesson&nbsp;
-	<select id="selLesson" onchange="$('#Pinyin').hide();$('#English').hide();LoadFlashCards();">
+	<select id="selLesson" name="selLesson" onchange="$('#Pinyin').hide();$('#English').hide();LoadFlashCards();">
+		<option value="All">All</option>
 		<option value="1">1</option>
 		<option value="2">2</option>
 		<option value="3">3</option>
@@ -38,15 +45,14 @@ include_once 'includes/db_connect.php';
 		<option value="13">13</option>
 		<option value="14">14</option>
 		<option value="15">15</option>
-		<option value="All">All</option>
-		<option value="1-3">1-3</option>
-		<option value="4-6">4-6</option>
-		<option value="7-9">7-9</option>
-		<option value="10-12">10-12</option>
-		<option value="13-15">13-15</option>
+		<option value="16">16</option>
+		<option value="17">17</option>
+		<option value="18">18</option>
+		<option value="19">19</option>
+		<option value="20">20</option>
 	</select>
 	<p class="chk">
-	<label class="checkbox-inline"><input type="checkbox" name="chkRANDOM" onclick="$('#Pinyin').hide();$('#English').hide();$('#PartofSpeech').hide(); displayCard();">随机</label>
+	<label class="checkbox-inline"><input type="checkbox" name="chkRANDOM" onclick="$('#Pinyin').hide();$('#English').hide();$('#PartofSpeech').hide(); dispCard();">随机</label>
 	<label class="pynD checkbox-inline"><input type="checkbox" name="chkPINYIN" onclick="pinyinChecked();">拼音</label>
 	<label class="engD checkbox-inline"><input type="checkbox" name="chkENGLISH" onclick="englishChecked();">英语</label>
 	</p>
@@ -60,33 +66,24 @@ include_once 'includes/db_connect.php';
 	</div>
 </div>
 <div class="footer">
-	<div class="ftrBtn">
-		<button type="button" class="btn btn-info" onclick="$('#Pinyin').hide();$('#English').hide();$('#PartofSpeech').hide();displayCard('first');">
-		<span class="glyphicon glyphicon-fast-backward"></span></button></div>
-	<div class="ftrBtn">
-		<button type="button" class="btn btn-info" onclick="$('#Pinyin').hide();$('#English').hide();$('#PartofSpeech').hide();displayCard('prev');">
-		<span class="glyphicon glyphicon-backward"></span></button></div>
-	<div id="footLeft"></div>
-	<div class="ftrBtn">
-		<button type="button" class="btn btn-info" onclick="$('#Pinyin').hide();$('#English').hide();$('#PartofSpeech').hide();displayCard('next');">
-		<span class="glyphicon glyphicon-forward"></span></button></div>
-	<div class="ftrBtn">
-		<button type="button" class="btn btn-info" onclick="$('#Pinyin').hide();$('#English').hide();$('#PartofSpeech').hide();displayCard('last');">
-		<span class="glyphicon glyphicon-fast-forward"></span></button></div>
+		<button type="button" class="btn btn-info btn-huge" onclick="$('#Pinyin').hide();$('#English').hide();$('#PartofSpeech').hide();dispCard('first');">
+		<span class="glyphicon glyphicon-fast-backward"></span></button>
+		<button type="button" class="btn btn-info btn-huge" onclick="$('#Pinyin').hide();$('#English').hide();$('#PartofSpeech').hide();dispCard('prev');">
+		<span class="glyphicon glyphicon-backward"></span></button>
+		<span id="footCenter"></span>
+		<button type="button" class="btn btn-info btn-huge" onclick="$('#Pinyin').hide();$('#English').hide();$('#PartofSpeech').hide();dispCard('next');">
+		<span class="glyphicon glyphicon-forward"></span></button>
+		<button type="button" class="btn btn-info btn-huge" onclick="$('#Pinyin').hide();$('#English').hide();$('#PartofSpeech').hide();dispCard('last');">
+		<span class="glyphicon glyphicon-fast-forward"></span></button>
+	<div class="clearfix"></div>
 	<div class="clearfix">
 		<button class="btn btn-default btn-pinyin btn-huge" onclick="$('#Pinyin').toggle();">拼音</button>
 		<button class="btn btn-default btn-english btn-huge" onclick="$('#English').toggle();$('#PartofSpeech').toggle();">英语</button>
 		<button class="btn btn-success btn-huge" onclick="$('#Pinyin').hide();$('#English').hide();$('#PartofSpeech').hide();knowIt();">
 			<span class="glyphicon glyphicon-ok"></span></button>
-		<button class="btn btn-danger btn-huge" onclick="$('#Pinyin').hide();$('#English').hide();$('#PartofSpeech').hide();displayCard();">
+		<button class="btn btn-danger btn-huge" onclick="$('#Pinyin').hide();$('#English').hide();$('#PartofSpeech').hide();dispCard();">
 			<span class="glyphicon glyphicon-remove"></span></button>
 	</div>
 </div>
-<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.12.2.min.js"></script>
-<script src="https://ajax.aspnetcdn.com/ajax/bootstrap/3.3.6/bootstrap.min.js"></script>
-<!--[if IE]>
-<script src="https://stephenmccready.asia/html5shiv.min.js"></script>
-<![endif]-->
-<script src="js/flash.js"></script>
 </body>
 </html>
