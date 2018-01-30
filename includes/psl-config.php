120fe3253ea0833dab6d9001f9e03f71663f3f4d
Stephen McCready <stephenmccready@gmail.com>
2018-01-30 18:20:26 +0800

    Update psl-config.php
    Fixed mis-leading comments

--- includes/psl-config.php
+++ includes/psl-config.php
@@ -1,8 +1,8 @@
 <?php
 define("HOST", "*** put your database location here ***");
 define("USER", "*** put your username here ***");
-define("PASSWORD", "*** put your username here ***"); 
-define("DATABASE", "*** put your password here ***");
+define("PASSWORD", "*** put your password here ***"); 
+define("DATABASE", "*** put your database name here ***");
 define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "user");
 define("SECURE", TRUE);
