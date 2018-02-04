231f201b2af4d1fa90718e71ddff7af5c51449fa
Stephen McCready <stephenmccready@gmail.com>
2018-02-04 19:47:08 +0800

    Update flash.js
    Added swipe left (previous card) and swipe right (next card) handlers

--- js/flash.js
+++ js/flash.js
@@ -179,3 +179,39 @@ function getCookie(cname) {
     }
     return "";
 }
+class Swipe {
+    constructor(element) {
+        this.xDown = null;
+        this.yDown = null;
+        this.element = typeof(element) === 'string' ? document.querySelector(element) : element;
+        this.element.addEventListener('touchstart', function(evt) {
+            this.xDown = evt.touches[0].clientX;
+            this.yDown = evt.touches[0].clientY;
+        }.bind(this), false);
+    }
+    onLeft(callback) {this.onLeft = callback;return this;}
+    onRight(callback) {this.onRight = callback;return this;}
+    onUp(callback) {this.onUp = callback;return this;}
+    onDown(callback) {this.onDown = callback;return this;}
+    handleTouchMove(evt) {
+        if ( ! this.xDown || ! this.yDown ) {
+            return;
+        }
+        var xUp = evt.touches[0].clientX;
+        var yUp = evt.touches[0].clientY;
+        this.xDiff = this.xDown - xUp;
+        this.yDiff = this.yDown - yUp;
+        if ( Math.abs( this.xDiff ) > Math.abs( this.yDiff ) ) {
+            if ( this.xDiff > 0 ) {this.onLeft();} else {this.onRight();}
+        } else {
+            if ( this.yDiff > 0 ) {this.onUp();} else {this.onDown();}
+        }
+        this.xDown = null;
+        this.yDown = null;
+    }
+    run() {this.element.addEventListener('touchmove', function(evt) {this.handleTouchMove(evt).bind(this);}.bind(this), false);}
+}
+var swiper = new Swipe('#Card');
+swiper.onLeft(function() { dispCard("prev") });
+swiper.onRight(function() { dispCard("next") });
+swiper.run();
