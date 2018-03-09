6ee3b6641bdd354b93d718d3787f961c0de65dc0
Stephen McCready <stephenmccready@gmail.com>
2018-03-09 08:27:41 +0800

    Update flash.js
    Added option to view as a list

--- js/flash.js
+++ js/flash.js
@@ -1,32 +1,42 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#efefef",
+      "text": "#404040"
+    },
+    "button": {
+      "background": "#8ec760",
+      "text": "#ffffff"
+    }
+  }
+})});
 var HSKLevelCookie;
 var HSKLessonCookie;
 function init() {
+	$('#toggleX').bootstrapToggle({
+		off: ' List',
+		on: ' Flashcards',
+		size: 'mini'
+	});
 	HSKLevelCookie=getCookie('HSKLevel');
 	HSKLessonCookie=getCookie('HSKLesson');
-	if(HSKLevelCookie=="") {
-		window.cookieconsent.initialise({
-  		"palette": {
-    		"popup": {
-      		"background": "#efefef",
-      		"text": "#404040"
-    		},
-    		"button": {
-     		"background": "#8ec760",
-      		"text": "#ffffff"
-    		}
-  		}
-		});
-	}
-	if(HSKLevelCookie==""){HSKLevelCookie='1';}
-	if(HSKLessonCookie==""){HSKLessonCookie='1';}
+	if(HSKLevelCookie===""){HSKLevelCookie='1';}
+	if(HSKLessonCookie===""){HSKLessonCookie='1';}
 	$("#selHSK").val(HSKLevelCookie);
 	$("#selLesson").val(HSKLessonCookie);
 	LoadFlashCards();
 }
+$('#myform :checkbox').change(function() {
+	if(this.id == "toggleX") {
+		$('#Card').toggle();
+		$('#fCard').toggle();
+		$('#pCard').toggle();
+		$('#List').toggle();
+	}
+});
 var resultArray;
 var ID=0;
-var deletedID=0;
-var deletedRow;
 function LoadFlashCards() {
 	$('.loader').show();
 	$('#HanziS').text('');
@@ -37,15 +47,16 @@ function LoadFlashCards() {
 	request.onreadystatechange = function() {
 		if (request.readyState === 4) {
 			if (request.status === 200) {
-				if(request.responseText=="") {
+				if(request.responseText==="") {
 					$('#HanziS').text("🚧");
 					$('#Pinyin').text('');
 					$('#PartofSpeech').text('');
-					$('#English').html("No cards for this lesson available yet.");
+					$('#English').html("No cards for this criteria available.");
 					$('#HanziS').show();
 					$('#English').show();
 					$('.loader').hide();
 					resultArray = [];
+					$('#List').html("🚧 <br/>No cards for this criteria available.");
 				} else {
 					var result = request.responseText;
 					var resultLen = result.length;
@@ -55,6 +66,7 @@ function LoadFlashCards() {
 					ID=0;
 					$('.loader').hide();
 					dispCard('first');
+					populateTable();
 				}
 			}
 		}
@@ -66,7 +78,8 @@ function LoadFlashCards() {
 function dispCard(action) {
 	var arrLen=resultArray.length;
 	$('#HanziS').text("");
-	if(arrLen==0) {
+	$('#OnRevList').text("");
+	if(arrLen===0) {
 		$('#HanziS').text("👍");
 		var hsk=$("#selHSK").val();
 		var l=$("#selLesson").val();
@@ -99,10 +112,11 @@ function dispCard(action) {
 			ID=0;
 		} else if($('input[name=chkRANDOM]:checked').val()) {
 			ID=Math.floor(Math.random() * arrLen);
-			if (arrLen!=0 && ID==lastID) {
-				if(ID==0) {ID++;} else {ID--;};
+			if (arrLen!==0 && ID===lastID) {
+				if(ID===0) {ID++;} else {ID--;}
 			}
 		} else if(action=="current"){
+		// Do nothing
 		} else {
 			ID++;
 			if(ID >= arrLen) {
@@ -113,25 +127,46 @@ function dispCard(action) {
 		var row=resultArray[ID];
 		lastID=ID;
 		var col=row.split("~");
-		$('#HanziS').text('');
 		for(x=0; x <= col[1].length; x++) {
-			$('#HanziS').append('<a class="strokeURL" href="https://hanyu.baidu.com/s?wd='+col[1].substring(x, 1)+'&ptype=zici" target="_blank">'+col[1].substring(x, 1)+'</a>');
+			if(col[1].substr(x, 1)=="(" || col[1].substr(x, 1)==")") {
+				$('#HanziS').append(col[1].substr(x, 1));
+			} else {
+				$('#HanziS').append('<a class="strokeURL" href="https://hanyu.baidu.com/s?wd='+col[1].substr(x, 1)+'&ptype=zici" target="_blank">'+col[1].substr(x, 1)+'</a>');
+			}
 		}
 		$('#Pinyin').text(col[3]);
 		$('#English').text(col[5]);
 		$('#PartofSpeech').text(col[6]);
-		col[10]++;
-		$('#Views').html('HSK ' + col[7] + '-' + col[8] + '&nbsp;&nbsp;&nbsp;Views: ' + col[10]);
+		$('#HSK').text(col[7]);
+		$('#DBID').text(col[9]);
+		col[12]++;
+		$('#Views').html('HSK ' + col[7] + '-' + col[8] + '&nbsp;&nbsp;&nbsp;Views: ' + col[12]);
 		var dispID=ID+1;
 		$('#footCenter').text(dispID.toString() + ' of ' + arrLen.toString());
 		resultArray[ID]=col[0]+'~'+col[1]+'~'+col[2]+'~'+col[3]+'~'+col[4]+'~'+col[5]+'~'+
-						col[6]+'~'+col[7]+'~'+col[8]+'~'+col[9]+'~'+col[10]+'~';
-		if($('input[name=chkPINYIN]:checked').val()) {$('#Pinyin').show();} else {$('#Pinyin').hide();};
-		if($('input[name=chkENGLISH]:checked').val()) {$('#English').show();} else {$('#English').hide();};
-		if($('input[name=chkENGLISH]:checked').val()) {$('#PartofSpeech').show();} else {$('#PartofSpeech').hide();};
+						col[6]+'~'+col[7]+'~'+col[8]+'~'+col[9]+'~'+col[10]+'~'+col[11]+'~'+col[12]+'~';	
+		if(col[10]!=='')	{
+			$('#Delete').html("<button class='btn btn-danger btn-xs' onclick='del("+col[10]+")'>Remove from list</button>");
+		} else {
+			$('#Delete').html("");
+		}
+		if($('input[name=chkPINYIN]:checked').val()) {$('#Pinyin').show();} else {$('#Pinyin').hide();}
+		if($('input[name=chkENGLISH]:checked').val()) {$('#English').show();} else {$('#English').hide();}
+		if($('input[name=chkENGLISH]:checked').val()) {$('#PartofSpeech').show();} else {$('#PartofSpeech').hide();}
 		setCookie("HSKLevel", $("#selHSK").val(), 120);
 		setCookie("HSKLesson", $("#selLesson").val(), 120);
 	}
+}
+function populateTable(){
+	$("#listTab tr").remove();
+	$("#wordcount").html("");
+	for (i = 0; i < resultArray.length; i++) {
+		var col=resultArray[i].split("~");
+    	$('#listTab').append('<tr><td>'+col[7]+'.'+col[8]+'</td><td>'+col[1]+'</td><td>'
+    		+col[3]+'</td><td><span class="eng">'+col[5]+'</span>&nbsp;<span class="posTab">'+col[6]+'</span></td></tr>');
+	}
+	$("#wordcount").html(resultArray.length+1);
+	$("#wordcount").append(" words");
 }
 function knowIt() {
 	resultArray.splice(ID, 1);
@@ -193,45 +228,35 @@ function getCookie(cname) {
         while (c.charAt(0) == ' ') {
         	c = c.substring(1);
         }
-        if (c.indexOf(name) == 0) {
+        if (c.indexOf(name) === 0) {
             return c.substring(name.length, c.length);
         }
     }
     return "";
 }
-class Swipe {
-    constructor(element) {
-        this.xDown = null;
-        this.yDown = null;
-        this.element = typeof(element) === 'string' ? document.querySelector(element) : element;
-        this.element.addEventListener('touchstart', function(evt) {
-            this.xDown = evt.touches[0].clientX;
-            this.yDown = evt.touches[0].clientY;
-        }.bind(this), false);
-    }
-    onLeft(callback) {this.onLeft = callback;return this;}
-    onRight(callback) {this.onRight = callback;return this;}
-    onUp(callback) {this.onUp = callback;return this;}
-    onDown(callback) {this.onDown = callback;return this;}
-    handleTouchMove(evt) {
-        if ( ! this.xDown || ! this.yDown ) {
-            return;
-        }
-        var xUp = evt.touches[0].clientX;
-        var yUp = evt.touches[0].clientY;
-        this.xDiff = this.xDown - xUp;
-        this.yDiff = this.yDown - yUp;
-        if ( Math.abs( this.xDiff ) > Math.abs( this.yDiff ) ) {
-            if ( this.xDiff > 0 ) {this.onLeft();} else {this.onRight();}
-        } else {
-            if ( this.yDiff > 0 ) {this.onUp();} else {this.onDown();}
-        }
-        this.xDown = null;
-        this.yDown = null;
-    }
-    run() {this.element.addEventListener('touchmove', function(evt) {this.handleTouchMove(evt).bind(this);}.bind(this), false);}
+function add() {	// Add to review list
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+		if (request.readyState === 4) {
+			if (request.status === 200) {
+				$('#Views').append(request.responseText);
+			}
+		}
+	};
+	var url='includes/insertIntoReview.php?id='+$('#DBID').text()+'&HSK='+$('#HSK').text();
+	request.open('GET', url, true);
+	request.send();
 }
-var swiper = new Swipe('#Card');
-swiper.onLeft(function() { dispCard("prev") });
-swiper.onRight(function() { dispCard("next") });
-swiper.run();
+function del(idToDel) {	// Remove from review list
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+		if (request.readyState === 4) {
+			if (request.status === 200) {
+				$('#Delete').html("");
+			}
+		}
+	};
+	var url='includes/deleteReview.php?id='+idToDel;
+	request.open('GET', url, true);
+	request.send();
+}
