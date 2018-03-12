c041470c628bfa1233105c3f98246f639f8d1a2b
Stephen McCready <stephenmccready@gmail.com>
2018-03-12 10:39:14 +0800

    Update loadFlashCards.php
    Added colors to pinyin tones

--- includes/loadFlashCards.php
+++ includes/loadFlashCards.php
@@ -1,42 +1,52 @@
 <?php
+//test: https://stephenmccready.asia/mi/includes/loadFlashCards.php?HSK=1&Lesson=1
 include_once 'db_connect.php';
-$sql="SELECT HanziS,HanziT,Pinyin,PinyinNum,English,PartofSpeech FROM Hanzi";
-if($_GET["HSK"]!="All") {
-	$sql.=" WHERE HSK = " . $_GET["HSK"];	
-	if($_GET["Lesson"]=="1-3") {
-		$sql.=  " AND Chapter In(1,2,3)";
-	} elseif($_GET["Lesson"]=="4-6") {
-		$sql.=  " AND Chapter In(4,5,6)";
-	} elseif($_GET["Lesson"]=="7-9") {
-		$sql.=  " AND Chapter In(7,8,9)";
-	} elseif($_GET["Lesson"]=="10-12") {
-		$sql.=  " AND Chapter In(10,11,12)";
-	} elseif($_GET["Lesson"]=="13-15") {
-		$sql.=  " AND Chapter In(13,14,15)";
+$sql="SELECT HanziS,HanziT,Pinyin,PinyinNum,English,PartofSpeech,H.HSK,Chapter,H.ID,R.ID As RevID FROM Hanzi As H ";
+if($_GET["Lesson"]=="Review") {
+	$sql.=" JOIN Review AS R ON R.ID=H.ID WHERE H.HSK=" . $_GET["HSK"];	
+} else if($_GET["HSK"]!="All") {
+	$sql.=" LEFT OUTER JOIN Review AS R ON R.ID=H.ID WHERE H.HSK = " . $_GET["HSK"];	
+	if($_GET["Lesson"]=="Verbs") {
+		$sql.= " AND PartOfSpeech = 'verb'";
+	} elseif($_GET["Lesson"]=="Adjectives") {
+		$sql.= " AND PartOfSpeech = 'adjective'";
+	} elseif($_GET["Lesson"]=="Adverbs") {
+		$sql.= " AND PartOfSpeech = 'adverb'";
 	} elseif($_GET["Lesson"]!="All") {
 		$sql.= " AND Chapter = " . $_GET["Lesson"];
 	}
 } else {
-	if($_GET["Lesson"]=="1-3") {
-		$sql.=  " WHERE Chapter In(1,2,3)";
-	} elseif($_GET["Lesson"]=="4-6") {
-		$sql.=  " WHERE Chapter In(4,5,6)";
-	} elseif($_GET["Lesson"]=="7-9") {
-		$sql.=  " WHERE Chapter In(7,8,9)";
-	} elseif($_GET["Lesson"]=="10-12") {
-		$sql.=  " WHERE Chapter In(10,11,12)";
-	} elseif($_GET["Lesson"]=="13-15") {
-		$sql.=  " WHERE Chapter In(13,14,15)";
+	if($_GET["Lesson"]=="Verbs") {
+		$sql.= " LEFT OUTER JOIN Review AS R ON R.ID=H.ID WHERE PartOfSpeech = 'verb'";
+	} elseif($_GET["Lesson"]=="Adjectives") {
+		$sql.= " LEFT OUTER JOIN Review AS R ON R.ID=H.ID WHERE PartOfSpeech = 'adjective'";
+	} elseif($_GET["Lesson"]=="Adverbs") {
+		$sql.= " LEFT OUTER JOIN Review AS R ON R.ID=H.ID WHERE PartOfSpeech = 'adverb'";
 	} else if($_GET["Lesson"]!="All") {
-		$sql.= " WHERE Chapter = " . $_GET["Lesson"];
+		$sql.= " LEFT OUTER JOIN Review AS R ON R.ID=H.ID WHERE Chapter = " . $_GET["Lesson"];
+	} else {
+		$sql.= " LEFT OUTER JOIN Review AS R ON R.ID=H.ID";
 	}
 }
+$sql.= " Order By H.HSK,CAST(Chapter AS UNSIGNED)";
 $result=mysqli_query($mysqli,$sql);
 mysqli_fetch_all($result,MYSQLI_ASSOC);
-foreach ($result as $key => $value) {
-	echo $key;
-	foreach($value as $key2 => $value2) {
-	    echo "~" . $value2;
+$rowNum=0;
+foreach ($result as $row) {
+	preg_match_all('!\d+!', $row["PinyinNum"], $num);
+	$PinyinNum="";
+	for($i=0; $i<=sizeof($num[0]); $i++) {
+		$PinyinNum = $PinyinNum . $num[0][$i];
 	}
-	echo '~0~0|';
+	$pinyinArray=explode(" ",$row["PinyinNum"]);
+	$PinyinSize="";
+	for($x=0; $x<sizeof($pinyinArray); $x++) {
+		$PinyinSize = $PinyinSize . strlen($pinyinArray[$x])-1;
+	}
+	echo $rowNum . "~" . $row["HanziS"] . "~" . $row["HanziT"] . "~" . $row["Pinyin"] . "~" . $PinyinNum . "~" . $row["English"] . "~" 
+		 . $row["PartofSpeech"] . "~" . $row["HSK"] . "~" . $row["Chapter"] . "~" . $row["ID"] . "~" . $row["RevID"] . "~0~0~" . $PinyinSize
+		 . "|";
+	$rowNum++;
 }
+
+$mysqli->close();
