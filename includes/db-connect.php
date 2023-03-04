17d1ec8aaae3616234b8c0eef9841e49123f2d51
removedporn <86824510+removedporn@users.noreply.github.com>
2023-03-04 20:18:04 +0800

    Delete includes directory

--- includes/db-connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-include_once 'psl-config.php';
-$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
-mysqli_set_charset($mysqli, "utf8");
--- includes/psl-config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("HOST", "*** put your database location here ***");
-define("USER", "*** put your username here ***");
-define("PASSWORD", "*** put your password here ***"); 
-define("DATABASE", "*** put your database name here ***");
-define("CAN_REGISTER", "any");
-define("DEFAULT_ROLE", "user");
-define("SECURE", TRUE);
